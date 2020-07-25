e596d2efc18038d343a17c21cc8d7f045cb1f276
Luba <luponom@mail.ru>
2020-07-25 17:55:30 +0300

    coors

--- controllers/users.js
+++ controllers/users.js
@@ -54,8 +54,7 @@ const login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        secure: false,
-        sameSite: true,
+        domain: 'mestopraktikum.xyz',
       });
       res.status(200).send({ token });
     })
