df548477db8d89e48122ba32f5db571ef59446a9
Luba <luponom@mail.ru>
2020-07-30 23:23:40 +0300

    samesite

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const { errorHandle } = require('./middlewares/errorHandle');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const corsOptions = {
-  origin: ['http://mestopraktikum.xyz', 'http://localhost:8080', 'https://ocfonta.github.io/news-explorer-frontend', 'https://ocfonta.github.io'],
+  origin: ['https://mestopraktikum.xyz', 'http://localhost:8080', 'https://ocfonta.github.io/news-explorer-frontend', 'https://ocfonta.github.io'],
   credentials: true,
 };
 
